6ad92e2460f475dea265cddc6a873f247795f1a4
Matteo Casonato <matteocasonato@protonmail.com>
2022-05-07 14:24:26 +0200

    new smart contract abi + address changed

--- lib/stateContext.dart
+++ lib/stateContext.dart
@@ -1,87 +1,86 @@
 import 'package:dart_web3/dart_web3.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class stateContext{
-   static stateContext? _stateContext;
-    String? _rpcUrl;
-    EthereumAddress? _contractAddr;
-    var _balance, _session, _account, _escrow, _credentials;
-    Barcode? _result;
-    String? _barCodeResult;
+class stateContext {
+  static stateContext? _stateContext;
+  String? _rpcUrl;
+  EthereumAddress? _contractAddr;
+  var _balance, _session, _account, _escrow, _credentials;
+  Barcode? _result;
+  String? _barCodeResult;
 
-    stateContext._internal(){
-     _rpcUrl = 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
+  stateContext._internal() {
+    _rpcUrl = 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
     _contractAddr =
-    EthereumAddress.fromHex('0xFDf572Ce0b87f7036f9F528FD55968348Bd86081');
-     _barCodeResult = "";
+        EthereumAddress.fromHex('0x7419970Bb76dD5bB893666368923d24f43191E61');
+    _barCodeResult = "";
   }
 
-  static stateContext getState(){
-    if(_stateContext == null){
+  static stateContext getState() {
+    if (_stateContext == null) {
       _stateContext = stateContext._internal();
     }
 
     return _stateContext!;
   }
 
-  String getRpcUrl(){
+  String getRpcUrl() {
     return _rpcUrl!;
   }
 
-  EthereumAddress getContractAddr(){
+  EthereumAddress getContractAddr() {
     return _contractAddr!;
   }
 
-  getBalance(){
+  getBalance() {
     return _balance;
   }
 
-  getSession(){
+  getSession() {
     return _session;
   }
 
-  setSession(session){
+  setSession(session) {
     _session = session;
   }
 
-  getAccount(){
+  getAccount() {
     return _account;
   }
 
-  setAccount(account){
+  setAccount(account) {
     _account = account;
   }
 
-  getEscrow(){
+  getEscrow() {
     return _escrow;
   }
 
-  setEscrow(escrow){
+  setEscrow(escrow) {
     _escrow = escrow;
   }
 
-  getCredentials(){
+  getCredentials() {
     return _credentials;
   }
 
-  setCredentials(credentials){
+  setCredentials(credentials) {
     _credentials = credentials;
   }
 
-  Barcode? getResult(){
+  Barcode? getResult() {
     return _result;
   }
 
-  setResult(result){
+  setResult(result) {
     _result = result;
   }
-  
-  String? getBarcodeResult(){
+
+  String? getBarcodeResult() {
     return _barCodeResult;
   }
 
-  setBarcodeResult(barCodeResult){
+  setBarcodeResult(barCodeResult) {
     _barCodeResult = barCodeResult;
   }
-  
 }
